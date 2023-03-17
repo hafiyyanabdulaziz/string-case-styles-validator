6ba5b481281e15237668686709eb50ab8dd2d33f
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2023-03-17 14:10:21 +0700

    coba kembalikan index ke root

---
+++ index.js
@@ -0,0 +1,21 @@
+import isCamelCase from "./src/isCamelCase.js";
+import isKebabCase from "./src/isKebabCase.js";
+import isPascalCase from "./src/isPascalCase.js";
+import isSnackCase from "./src/isSnackCase.js";
+import {
+  toCamelCase,
+  toKebabCase,
+  toPascalCase,
+  toSnackCase,
+} from "./src/convert.js";
+
+export {
+  isCamelCase,
+  isKebabCase,
+  isPascalCase,
+  isSnackCase,
+  toCamelCase,
+  toKebabCase,
+  toPascalCase,
+  toSnackCase,
+};
